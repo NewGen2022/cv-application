c8153aa9ed606852867b83fb2c315749960b4980
NewGen2022 <amirgimari@gmail.com>
2024-05-30 20:54:33 +0300

    set hot module replacement and open web-site on start

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cv-application/",
-  plugins: [react()],
-})
+    base: '/cv-application/',
+    plugins: [react()],
+    server: {
+        hot: true,  // Enable Hot Module Replacement
+        open: true, // Automatically open the browser on server start
+    }
+});

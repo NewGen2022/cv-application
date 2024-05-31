636191595dfad1b279ba0be275f1f6d2ca03de5c
NewGen2022 <amirgimari@gmail.com>
2024-05-31 21:20:48 +0300

    deleted togglehandler

--- src/components/sidebar/FormHeader.jsx
+++ src/components/sidebar/FormHeader.jsx
@@ -1,18 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp  } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const FormHeader = ({ icon, headerText }) => {
-    const [isVisible, setIsVisible] = useState(true);
-
-    const toggleHandler = (event) => {
-        event.preventDefault();
-        setIsVisible(!isVisible);
-        console.log(headerText);
-    };
-
-
+const FormHeader = ({ icon, headerText, isVisible, toggleHandler }) => {
     return (
         <div className='header'>
             <FontAwesomeIcon icon={icon} className='icon' />
@@ -27,6 +17,8 @@ const FormHeader = ({ icon, headerText }) => {
 FormHeader.propTypes = {
     icon: PropTypes.object.isRequired,
     headerText: PropTypes.string.isRequired,
+    isVisible: PropTypes.bool.isRequired,
+    toggleHandler: PropTypes.func.isRequired,
 };
 
 export default FormHeader;

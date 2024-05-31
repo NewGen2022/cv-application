e1d384274e7f7c734d1c8b12e32341a5c99718b6
NewGen2022 <amirgimari@gmail.com>
2024-05-31 10:02:34 +0300

    added onclick simulation

--- src/components/sidebar/TopBar.jsx
+++ src/components/sidebar/TopBar.jsx
@@ -1,9 +1,21 @@
 const TopBar = () => {
+    const handleClear = () => {
+        console.log('Clear Resume clicked');
+    };
+
+    const handleLoadExample = () => {
+        console.log('Load Example clicked');
+    };
+
+    const handleDownload = () => {
+        console.log('Download clicked');
+    };
+
     return (
         <div id='top-bar'>
-            <button id='clear'>Clear Resume</button>
-            <button id='load-example'>Load Example</button>
-            <button id='download'>Download</button>
+            <button id='clear' onClick={handleClear}>Clear Resume</button>
+            <button id='load-example' onClick={handleLoadExample}>Load Example</button>
+            <button id='download' onClick={handleDownload}>Download</button>
         </div>
     );
 };

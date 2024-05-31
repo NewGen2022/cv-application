0ba57f52dd32198ea93ab67cd5689c3f82cb1bf5
NewGen2022 <amirgimari@gmail.com>
2024-05-31 11:30:21 +0300

    added name attribute to form input

--- src/components/sidebar/FormInput.jsx
+++ src/components/sidebar/FormInput.jsx
@@ -4,13 +4,14 @@ const FormInput = ({ id, labelText, type='text', placeholder, required=false }) 
     return (
         <div className="form-input">
             <label htmlFor={id}>{labelText}</label>
-            <input id={id} type={type} placeholder={placeholder} required={required}/>
+            <input id={id} name={id} type={type} placeholder={placeholder} required={required}/>
         </div>
     );
 };
 
 FormInput.propTypes = {
     id: PropTypes.string.isRequired,
+    name: PropTypes.string,
     labelText: PropTypes.string.isRequired,
     type: PropTypes.string,
     placeholder: PropTypes.string.isRequired,

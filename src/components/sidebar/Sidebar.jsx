54ae58545267eb7d19394f5de9a4f596ccfc094d
NewGen2022 <amirgimari@gmail.com>
2024-06-04 11:03:53 +0300

    added handlers for: input changing, input submitting, delete institution; institutions

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,18 +2,31 @@ import TopBar from './TopBar';
 import Personal from './Personal';
 import Education from './Education';
 import Experience from './Experience';
+import PropTypes from 'prop-types';
 
-const Sidebar = () => {
+const Sidebar = ({ handleInputChange, handleInputSubmit, institutions, onDelete }) => {
     return (
         <div id='sidebar'>
             <TopBar />
             <div id='forms'>
-                <Personal />
-                <Education />
-                <Experience />
+                <Personal handleInputChange={handleInputChange} />
+                <Education 
+                    handleInputChange={handleInputChange}
+                    handleInputSubmit={handleInputSubmit} 
+                    institutions={institutions}
+                    onDelete={onDelete}
+                />
+                <Experience handleInputChange={handleInputChange} handleInputSubmit={handleInputSubmit} />
             </div>
         </div>
     );
+};
+
+Sidebar.propTypes = {
+    handleInputChange: PropTypes.func.isRequired,
+    handleInputSubmit: PropTypes.func.isRequired,
+    institutions: PropTypes.array,
+    onDelete: PropTypes.func.isRequired
 };
 
 export default Sidebar;

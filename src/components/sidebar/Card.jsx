3f5a986a6fc4441e606ee6f070049a55976a1db4
NewGen2022 <amirgimari@gmail.com>
2024-06-04 21:08:40 +0300

    added getting first parameter from object

--- src/components/sidebar/Card.jsx
+++ src/components/sidebar/Card.jsx
@@ -7,10 +7,12 @@ const Card = ({ institution, onDelete }) => {
         event.preventDefault();
         onDelete(institution);
     };
-    
+
+    const firstKey = Object.keys(institution)[0];
+
     return (
         <div className="card-wrapper">
-            <span>{institution['school-uni']}</span>
+            <span>{institution[firstKey]}</span>
             <button onClick={handleDelete} className="delete-button">
                 <FontAwesomeIcon icon={faTrash} />
             </button>

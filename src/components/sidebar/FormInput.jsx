0ca752a721d06b698c11bbc659c32625117d6a86
NewGen2022 <amirgimari@gmail.com>
2024-06-04 10:43:13 +0300

    added default props

--- src/components/sidebar/FormInput.jsx
+++ src/components/sidebar/FormInput.jsx
@@ -1,21 +1,33 @@
 import PropTypes from 'prop-types';
 
-const FormInput = ({ id, labelText, type='text', placeholder, required=false }) => {
+const FormInput = ({ id, labelText, type, placeholder, required=false, onInputChange }) => {
     return (
         <div className="form-input">
             <label htmlFor={id}>{labelText}</label>
-            <input id={id} name={id} type={type} placeholder={placeholder} required={required}/>
+            <input 
+                id={id} 
+                name={id} 
+                type={type} 
+                placeholder={placeholder} 
+                required={required}
+                onChange={onInputChange}
+            />
         </div>
     );
+};
+
+FormInput.defaultProps = {
+    type: 'text'
 };
 
 FormInput.propTypes = {
     id: PropTypes.string.isRequired,
     name: PropTypes.string,
-    labelText: PropTypes.string.isRequired,
     type: PropTypes.string,
+    labelText: PropTypes.string.isRequired,
     placeholder: PropTypes.string.isRequired,
     required: PropTypes.bool,
+    onInputChange: PropTypes.func.isRequired,
 };
 
 export default FormInput;

2e29a18ba1ea2d9fda23ee2a96725f7ee43ba71d
NewGen2022 <amirgimari@gmail.com>
2024-06-04 10:56:03 +0300

    added displaying of personal details and education

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,32 @@
 import ResumeHeader from './ResumeHeader';
+import OutputFields from './OutputFields';
 
-const Resume = () => {
+const Resume = (formsData) => {
+    console.log('12345678', formsData);
     return (
         <div id='resume'>
-            <ResumeHeader />
+            <ResumeHeader 
+                name={formsData.name}
+                email={formsData.email}
+                phone={formsData.phone}
+                address={formsData.address}
+            />
+            <div className='resume-body'>
+            
+                <div className='education-wrapper'>
+                    <OutputFields className='education-header' data='Education'/>
+                    {formsData.institutions.map((institution, index) => (
+                        <div key={index} className='school-uni-wrapper'>
+                            {Object.entries(institution).map(([key, value]) => (
+                                <OutputFields key={key} className={key} data={value}/>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+
+
+                <OutputFields className='experience-header' data='Professional Experience'/>
+            </div>
         </div>
     );
 };

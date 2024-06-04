dfe8dea1c0bf24dd1c40acd7345289d76ccdda4a
NewGen2022 <amirgimari@gmail.com>
2024-06-04 10:46:16 +0300

    added handleinputchange as prop

--- src/components/sidebar/Personal.jsx
+++ src/components/sidebar/Personal.jsx
@@ -1,9 +1,10 @@
-import FormInput from './FormInput';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-import FormHeader from './FormHeader';
 import { useState } from 'react';
+import PropTypes from 'prop-types';
+import FormInput from './FormInput';
+import FormHeader from './FormHeader';
 
-const Personal = () => {
+const Personal = ({ handleInputChange }) => {
     const [isVisible, setIsVisible] = useState(true);
 
     const showPersonalForm = (event) => {
@@ -21,14 +22,19 @@ const Personal = () => {
             />
             {isVisible && (
                 <>
-                    <FormInput id='name' labelText='Full Name' placeholder='Enter First and Last Name' required />
-                    <FormInput id='email' labelText='Email' type='email' placeholder='Enter email' />
-                    <FormInput id='phone' labelText='Phone number' type='tel' placeholder='Enter phone number' />
-                    <FormInput id='address' labelText='Address' placeholder='City, Country' />
+                    <FormInput id='name' labelText='Full Name' placeholder='Enter First and Last Name' onInputChange={handleInputChange} required />
+                    <FormInput id='email' labelText='Email' type='email' placeholder='Enter email' onInputChange={handleInputChange} />
+                    <FormInput id='phone' labelText='Phone number' type='tel' placeholder='Enter phone number' onInputChange={handleInputChange} />
+                    <FormInput id='address' labelText='Address' placeholder='City, Country' onInputChange={handleInputChange} />
                 </>
             )}
         </form>
     );
+};
+
+
+Personal.propTypes = {
+    handleInputChange: PropTypes.func.isRequired,
 };
 
 export default Personal;

d2a56e1234036685689c5fc530891262ac11b8d7
NewGen2022 <amirgimari@gmail.com>
2024-06-06 16:57:15 +0300

    added logic for load example and clear handlers

--- src/components/sidebar/TopBar.jsx
+++ src/components/sidebar/TopBar.jsx
@@ -1,23 +1,67 @@
-const TopBar = () => {
+import PropTypes from 'prop-types';
+
+const TopBar = ({ setFormData, setEducationInstitution, setExperience }) => {
     const handleClear = () => {
-        console.log('Clear Resume clicked');
+        setFormData({});
+        setEducationInstitution([]);
+        setExperience([]);
     };
 
     const handleLoadExample = () => {
-        console.log('Load Example clicked');
-    };
-
-    const handleDownload = () => {
-        console.log('Download clicked');
+        setFormData({
+            address: '123 Sakura St, Tokyo, Japan',
+            email: 'satoshi.nakamoto@example.jp',
+            name: 'Satoshi Nakamoto',
+            phone: '(81) 90-1234-5678',
+        });
+        setEducationInstitution([
+            {
+                degree: 'Bachelor of Science in Information Technology',
+                schoolUni: 'University of Tokyo',
+                location: 'Tokyo, Japan',
+                startDate: 'April 2015',
+                endDate: 'March 2019',
+            },
+            {
+                degree: 'Master of Science in Artificial Intelligence',
+                schoolUni: 'Kyoto University',
+                location: 'Kyoto, Japan',
+                startDate: 'April 2019',
+                endDate: 'March 2021',
+            }
+        ]);
+        setExperience([
+            {
+                positionTitle: 'Software Engineer',
+                companyName: 'Nippon Tech Solutions',
+                description: 'Developed and maintained web applications using React and Node.js.',
+                locationExp: 'Tokyo, Japan',
+                startDateExp: 'April 2021',
+                endDateExp: 'Present',
+            },
+            {
+                positionTitle: 'Intern Software Developer',
+                companyName: 'Samurai Apps LLC',
+                description: 'Assisted in the development of mobile applications and performed software testing.',
+                locationExp: 'Osaka, Japan',
+                startDateExp: 'June 2018',
+                endDateExp: 'August 2018',
+            }
+        ]);
     };
 
     return (
         <div id='top-bar'>
             <button id='clear' onClick={handleClear}>Clear Resume</button>
             <button id='load-example' onClick={handleLoadExample}>Load Example</button>
-            <button id='download' onClick={handleDownload}>Download</button>
         </div>
     );
+};
+
+TopBar.propTypes = {
+    setFormData: PropTypes.func.isRequired,
+    setEducationInstitution: PropTypes.func.isRequired,
+    setExperience: PropTypes.func.isRequired,
 };
 
 export default TopBar;
